341042d7ff0d427de0d8ca9dc23842b2d772a220
Marcio Woitek <mwoitekjr@gmail.com>
2025-12-20 20:47:48 -0300

    Day 1: Solution to part 1

---
+++ 01/pt1.d
@@ -0,0 +1,45 @@
+import std.algorithm.iteration : map, sum;
+import std.algorithm.sorting : sort;
+import std.format.read : formattedRead;
+import std.math.algebraic : abs;
+import std.range : zip;
+import std.stdio : File, writeln;
+
+struct Lists {
+    long[] list1;
+    long[] list2;
+
+    long totalDistance() const
+    {
+        return zip(list1, list2).map!(a => abs(a[0] - a[1])).sum();
+    }
+}
+
+Lists readLists(in string inputPath)
+{
+    long[] list1;
+    long[] list2;
+
+    long num1;
+    long num2;
+
+    auto file = File(inputPath, "r");
+    foreach (line; file.byLine) {
+        formattedRead(line, "%d   %d", &num1, &num2);
+        list1 ~= num1;
+        list2 ~= num2;
+    }
+
+    list1.sort();
+    list2.sort();
+
+    return Lists(list1, list2);
+}
+
+void main(string[] args)
+{
+    const string inputPath = args[1];
+    const Lists lists = readLists(inputPath);
+    const long totalDistance = lists.totalDistance();
+    writeln("Total distance: ", totalDistance);
+}
