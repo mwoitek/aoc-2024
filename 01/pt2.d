e5838661829bcebf4113f2bc08674fc6a23a5cb2
Marcio Woitek <mwoitekjr@gmail.com>
2026-01-08 12:02:57 -0300

    Day 1: Improve solution to part 2

--- 01/pt2.d
+++ 01/pt2.d
@@ -1,65 +1,76 @@
-import std.algorithm.iteration : filter, map, sum;
-import std.datetime.stopwatch : AutoStart, StopWatch;
-import std.format.read : formattedRead;
-import std.stdio : File, writef, writeln;
+import std.container : Array;
+
+// wc -l day_01.txt
+const ulong MAX_NUMS = 1000;
 
 struct Lists {
-    ulong[] left;
-    ulong[] right;
+    Array!ulong left;
+    Array!ulong right;
 }
 
 Lists readLists(in string inputPath)
 {
-    ulong[] left;
-    ulong[] right;
+    import std.format.read : formattedRead;
+    import std.stdio : File;
 
+    Array!ulong left;
+    Array!ulong right;
     ulong num1;
     ulong num2;
-
+    left.reserve(MAX_NUMS);
+    right.reserve(MAX_NUMS);
     auto file = File(inputPath, "r");
     foreach (line; file.byLine) {
         formattedRead(line, "%u   %u", &num1, &num2);
-        left ~= num1;
-        right ~= num2;
+        left.insertBack(num1);
+        right.insertBack(num2);
     }
-
     return Lists(left, right);
 }
 
-ulong[ulong] getNumberCounts(in ulong[] numbers)
+ulong[ulong] getNumberCounts(in Array!ulong numbers)
 {
     ulong[ulong] counts;
     foreach (number; numbers) {
-        if (number in counts) {
-            counts[number] += 1;
-        } else {
+        if (number in counts)
+            counts[number]++;
+        else
             counts[number] = 1;
-        }
     }
     return counts;
 }
 
-ulong similarityScore(in ulong[] left, in ulong[] right)
+ulong similarityScore(in Array!ulong left, in Array!ulong right)
 {
-    const auto countsLeft = getNumberCounts(left);
-    const auto countsRight = getNumberCounts(right);
-    return countsLeft.byKeyValue().filter!(a => a.key in countsRight)
+    import std.algorithm.iteration : filter, map, sum;
+
+    const auto countsLeft = left.getNumberCounts;
+    const auto countsRight = right.getNumberCounts;
+    return countsLeft.byKeyValue
+        .filter!(a => a.key in countsRight)
         .map!(a => a.key * a.value * countsRight[a.key])
-        .sum();
+        .sum;
 }
 
 void main(string[] args)
 {
-    const auto inputPath = args[1];
+    import core.memory : GC;
+
+    GC.disable;
+
+    import std.datetime.stopwatch : AutoStart, StopWatch;
+    import std.stdio : writef, writeln;
+
+    const string inputPath = args[1];
     auto sw = StopWatch(AutoStart.no);
 
-    sw.start();
-    const auto lists = readLists(inputPath);
-    const auto score = similarityScore(lists.left, lists.right);
-    sw.stop();
+    sw.start;
+    const Lists lists = inputPath.readLists;
+    const ulong score = similarityScore(lists.left, lists.right);
+    sw.stop;
 
     writeln("Similarity score: ", score);
 
-    const auto execTime = sw.peek.total!"usecs" / 1000.0;
+    const double execTime = sw.peek.total!"usecs" / 1000.0;
     writef("Execution time (ms): %.3f\n", execTime);
 }
